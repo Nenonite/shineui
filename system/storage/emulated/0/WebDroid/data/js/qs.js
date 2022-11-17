ac3daa26172b525a91e817708b71f42c3baf508a
Shine Industries <116729113+ShineIndustries@users.noreply.github.com>
2022-11-17 10:27:51 +0300

    Add files via upload

--- system/storage/emulated/0/WebDroid/data/js/qs.js
+++ system/storage/emulated/0/WebDroid/data/js/qs.js
@@ -1,7 +1,20 @@
 function open_qs() {
-     document.getElementById('qs_bg').classList.remove('hide');
+     document.getElementById('qs').classList.remove('hideqs');
+     document.getElementById('qs').classList.add('showqs');
+     document.getElementById('hide_qs_button').classList.remove('hide');
+     document.getElementById('start-menu').classList.add('smhide');
+     document.getElementById('start-menu').classList.remove('smshow');
 }
 
 function hide_qs() {
-     document.getElementById('qs_bg').classList.add('hide');
+     document.getElementById('qs').classList.remove('showqs');
+     document.getElementById('qs').classList.add('hideqs');
+     document.getElementById('hide_qs_button').classList.add('hide');
+     document.getElementById('start-menu').classList.add('smhide');
+     document.getElementById('start-menu').classList.remove('smshow');
+}
+
+function show_sm() {
+     document.getElementById('start-menu').classList.remove('smhide');
+     document.getElementById('start-menu').classList.add('smshow');
 }
